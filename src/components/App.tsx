6793e296e2aa1a47269db0bdd5ad4f92ba68509d
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-09-28 22:21:48 +0200

    create cafeInfo component
    add css

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+import css from "./App.module.css";
+import CafeInfo from "./CafeInfo";
+
+export default function App() {
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+    </div>
+  );
+}

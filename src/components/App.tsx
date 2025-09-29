24bdf93f911357e626ba856cb62b038d661ee82f
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-09-29 10:44:43 +0200

    add vote option component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 // import { useState } from "react";
 import css from "./App.module.css";
 import CafeInfo from "./CafeInfo";
+import VoteOptions from "./VoteOptions";
 
 export default function App() {
   return (
     <div className={css.app}>
       <CafeInfo />
+      <VoteOptions />
     </div>
   );
 }

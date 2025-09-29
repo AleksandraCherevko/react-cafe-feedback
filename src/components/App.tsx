1445987656d64f7b133434c4dc15067a0e863193
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-09-29 10:52:28 +0200

    add vote stats component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,14 @@
 import css from "./App.module.css";
 import CafeInfo from "./CafeInfo";
 import VoteOptions from "./VoteOptions";
+import VoteStats from "./VoteStats";
 
 export default function App() {
   return (
     <div className={css.app}>
       <CafeInfo />
       <VoteOptions />
+      <VoteStats />
     </div>
   );
 }
